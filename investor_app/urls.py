6f001eee29525f1ae7be19ac7c134e6e0162954c
Abby966 <abegail.chanyalew-ug@aau.edu.et>
2025-11-20 10:18:51 +0300

    Update urls.py

--- investor_app/urls.py
+++ investor_app/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
+from django.views.generic import TemplateView
+from django.urls import re_path
+
+
+urlpatterns += [
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'), name='frontend'),
+]
+
 from .views import (
     # Auth
     SignupView, 
